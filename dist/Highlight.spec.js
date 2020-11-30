0a6961a2b3afbab55b56463d983c85205e75a8f7
Alberto Casado Torres <acasadotorres@gmail.com>
2020-11-30 15:43:31 +0100

    updated dependencies

--- dist/Highlight.spec.js
+++ dist/Highlight.spec.js
@@ -11,31 +11,36 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 describe('Highlight', () => {
-  it('should render a rectangle', () => {
-    const highlight = {
+  let highlight;
+  let highlight2;
+  let highlight3;
+  beforeEach(() => {
+    highlight = {
       top: 0,
       left: 0,
       width: 0,
       height: 0,
       regionId: "1"
     };
-    const highlight2 = {
+    highlight2 = {
       top: 0,
       left: 0,
       width: 0,
       height: 0,
       regionId: "1"
     };
-    const highligh3 = {
+    highlight3 = {
       top: 1,
       left: 1,
       width: 1,
       height: 1,
       regionId: "2"
     };
+  });
+  it('should render a rectangle', () => {
     const selectionHandlerWrapper = (0, _enzyme.shallow)( /*#__PURE__*/React.createElement(_Highlight.Highlight, {
       highlight: {
-        selectionRectangles: [highlight, highlight2, highligh3]
+        selectionRectangles: [highlight, highlight2, highlight3]
       },
       regionId: "1"
     }));
@@ -49,6 +54,14 @@ describe('Highlight', () => {
     });
     expect(selectionHandlerWrapper.find('div').at(0).hasClass('highlight-rectangle')).toEqual(true);
     expect(selectionHandlerWrapper.find('div').length).toEqual(2);
+  });
+  it('should render all highlights of regionId is not provided', () => {
+    const selectionHandlerWrapper = (0, _enzyme.shallow)( /*#__PURE__*/React.createElement(_Highlight.Highlight, {
+      highlight: {
+        selectionRectangles: [highlight, highlight2, highlight3]
+      }
+    }));
+    expect(selectionHandlerWrapper.find('div').length).toEqual(3);
   });
   it('should render two rectangles', () => {
     const highlights1 = {
