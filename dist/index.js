2ae3f5c23b9d679b3b53b07063a059d710a3f5c4
GabrielPiles <gabriel.piles.glez@gmail.com>
2021-01-28 17:56:06 +0100

    Add sourcemap to build

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SelectionRegion = exports.SelectionHandler = exports.Highlight = void 0;
-var Highlight_1 = require("./Highlight");
-Object.defineProperty(exports, "Highlight", { enumerable: true, get: function () { return Highlight_1.Highlight; } });
-var SelectionHandler_1 = require("./SelectionHandler");
-Object.defineProperty(exports, "SelectionHandler", { enumerable: true, get: function () { return SelectionHandler_1.SelectionHandler; } });
-var SelectionRegion_1 = require("./SelectionRegion");
-Object.defineProperty(exports, "SelectionRegion", { enumerable: true, get: function () { return SelectionRegion_1.SelectionRegion; } });
